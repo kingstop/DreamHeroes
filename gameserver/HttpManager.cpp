02390e622f32bde2c451a25d4ffcae64c7da3f90
kingstop <kingstop@sina.com>
2016-11-03 19:30:24 +0800

    add

--- gameserver/HttpManager.cpp
+++ gameserver/HttpManager.cpp
@@ -215,12 +215,12 @@ bool VerifyDealHttpTaskIOS::excute()
 		std::string respone_url;
 		unsigned char sz_temp[10240];
 		unsigned char sz_temp_target[10240];
+		urlencode((unsigned char*)_receipt.c_str(), sz_temp_target);
 		sprintf((char*)sz_temp, "http://121.43.187.139:8080/paygateway/index.php?action=third_confirm&channel_id=%d&game_id=%d&user_id=%llu&order_id=%d&receipt={%s}",
-			channel_id, game_id, _acc, _order_id, _receipt.c_str());
-		urlencode(sz_temp, sz_temp_target);
+			channel_id, game_id, _acc, _order_id, sz_temp_target);		
 		Mylog::log_server(LOG_INFO, "http verify [%s] ", sz_temp);
 		Mylog::log_server(LOG_INFO, "http encode verify [%s] ", sz_temp_target);
-		gHttpManager.Posts((char*)sz_temp_target, post_url, respone_url);
+		gHttpManager.Posts((char*)sz_temp, post_url, respone_url);
 		try
 		{
 			bool bret = false;
