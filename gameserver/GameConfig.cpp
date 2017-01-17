1b9439dc66f09dce99cfc4178d40ed0e23d47248
kingstop <kingstop@sina.com>
2017-01-17 11:50:13 +0800

    add

--- gameserver/GameConfig.cpp
+++ gameserver/GameConfig.cpp
@@ -273,10 +273,10 @@ bool GameConfig::isInToday(u32 time)
 	p1->tm_min = 0;
 	p1->tm_sec = 0;
 	p1->tm_hour = _global_config.day_Refresh_time_;
-	time_t today_refresh_time =  mktime(p1) - 8 * 60 * 60; //utf 时间差
-	time_t Yesterday_refresh_time = mktime(p1) - (8 + 24) * 60 * 60 ;
+	time_t today_refresh_time =  mktime(p1) - (8 * 60 * 60); //utf 时间差
+	time_t Tomorrow_refresh_time = today_refresh_time + (24 * 60 * 60 );
 	bool ret = false;
-	if (time > Yesterday_refresh_time && time <= today_refresh_time)
+	if (time < Tomorrow_refresh_time && time >= today_refresh_time)
 	{
 		ret = true;
 	}
